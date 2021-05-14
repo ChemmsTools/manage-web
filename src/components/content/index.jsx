b1852b192cb1c8522889748fe0e6427b806b2b70
GUI <541349328@qq.com>
2021-05-14 23:21:42 +0800

    feat: 开发右侧内容组件

---
+++ src/components/content/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.scss'
+
+export default class Content extends React.Component {
+    render() {
+        return (
+            <div className="content-container">
+                <div className="content-main">
+                    <p>Hello,World!</p>
+                </div>
+            </div>
+        )
+    }
+}
