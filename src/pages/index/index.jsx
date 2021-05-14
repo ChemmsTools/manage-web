1a859788eb618dd18f45263152dca8fdc966f7a7
GUI <541349328@qq.com>
2021-05-14 23:21:22 +0800

    feat: 开发后台管理页面

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,16 +1,53 @@
-import React from 'react';
+import React from 'react'
+import Header from '../../components/header'
+import Sider from '../../components/sider'
+import Content from '../../components/content'
+import Login from '../../components/login'
 import './index.scss'
 
 export default class Index extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLogin: false
+        }
+    }
 
-  render() {
-    return (
-      <div className='container'>脚手架模板</div>
-    );
-  }
+    changeLogin = (isLogin) => {
+        this.setState({ isLogin })
+    }
+
+    shouldComponentUpdate() {
+        const { isLogin } = this.state
+        const main = document.getElementById("main")
+        const login = document.getElementById("login-container")
+
+        if (isLogin) {
+            login.style.display = 'flex'
+            main.style.display = 'none'
+        } else {
+            main.style.display = 'flex'
+            login.style.display = 'none'
+        }
+
+        return true
+    }
+
+
+    render() {
+        const { isLogin } = this.state
+
+        return (
+            <div className="index-container">
+                <Header isLogin={isLogin} getHeaderValue={this.changeLogin} />
+                <div className="main" id="main">
+                    <div className="left">
+                        <Sider />
+                    </div>
+                    <Content />
+                </div>
+                <Login getLoginValue={this.changeLogin} />
+            </div>
+        )
+    }
 }
