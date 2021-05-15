0dee0f91557b76897cebbce280d71e3777122224
GUI <541349328@qq.com>
2021-05-15 18:13:07 +0800

    feat: 实现Sider和Content组件通信

--- src/components/content/index.jsx
+++ src/components/content/index.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import PubSub from 'pubsub-js'
 import './index.scss'
 
 export default class Content extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            text: 'Hello,World!'
+        }
+    }
+
+    componentDidMount() {
+        // 从 Sider 组件订阅信息，展示对应的内容
+        PubSub.subscribe('siderToContent', (msg, text) => {
+            this.setState({ text })
+        })
+    }
+
     render() {
+        const { text } = this.state
         return (
             <div className="content-container">
                 <div className="content-main">
-                    <p>Hello,World!</p>
+                    <p>{text}</p>
                 </div>
             </div>
         )
--- src/components/sider/index.jsx
+++ src/components/sider/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Menu } from 'antd';
 import { AppstoreOutlined, DatabaseOutlined } from '@ant-design/icons';
+import PubSub from 'pubsub-js'
 import 'antd/dist/antd.css'
 import './index.scss'
 
@@ -18,6 +19,7 @@ export default class Sider extends React.Component {
         }
     }
 
+    // 管理菜单的展开闭合
     // keys：当前展开的 SubMenu 菜单项 key 数组
     onOpenChange = (keys) => {
         const { allSubmenuKeys, openKeys } = this.state
@@ -32,6 +34,12 @@ export default class Sider extends React.Component {
         }
     };
 
+    // 点击后 Content 组件进行对应内容响应
+    showContent = (m) => {
+        // 向 Content 组件传递选择的内容，使其展示对应信息
+        PubSub.publish('siderToContent', m.key)
+    }
+
 
     render() {
         const { openKeys } = this.state
@@ -40,16 +48,12 @@ export default class Sider extends React.Component {
             <div className="sider-container">
                 <Menu mode="inline" openKeys={openKeys} onOpenChange={this.onOpenChange} style={{ width: "100%" }}>
                     <SubMenu key="tools" icon={<DatabaseOutlined />} title="工具">
-                        <Menu.Item key="tools_1">Option 1</Menu.Item>
-                        <Menu.Item key="tools_2">Option 2</Menu.Item>
+                        <Menu.Item key="tools_1" onClick={this.showContent}>Option 1</Menu.Item>
+                        <Menu.Item key="tools_2" onClick={this.showContent}>Option 2</Menu.Item>
                     </SubMenu>
                     <SubMenu key="components" icon={<AppstoreOutlined />} title="组件">
-                        <Menu.Item key="components_1">Option 3</Menu.Item>
-                        <Menu.Item key="components_2">Option 4</Menu.Item>
-                        <SubMenu key="components_3" title="小组件">
-                            <Menu.Item key="components_3_1">Option 5</Menu.Item>
-                            <Menu.Item key="components_3_2">Option 6</Menu.Item>
-                        </SubMenu>
+                        <Menu.Item key="components_1" onClick={this.showContent}>Option 3</Menu.Item>
+                        <Menu.Item key="components_2" onClick={this.showContent}>Option 4</Menu.Item>
                     </SubMenu>
                 </Menu>
             </div>
