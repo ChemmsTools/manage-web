556300d45bee76eb48a783bc52db8ebfdac4487b
GUI <541349328@qq.com>
2021-05-15 14:22:39 +0800

    feat: 优化组件嵌套关系、样式，采用订阅发布传值

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Input, Button } from 'antd';
+import { Input, Button, message } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
+import PubSub from 'pubsub-js'
 import MD5 from 'md5.js'
 import './index.scss'
 
@@ -24,24 +25,48 @@ export default class Login extends React.Component {
     }
 
     login = () => {
-        if (1) {
-            console.log(1);
-            this.props.getLoginValue(false)
+        const { username, password } = this.state
+
+        // 防止吐丝，每次先销毁之前的
+        message.destroy()
+        message.config({
+            top: 150,
+            duration: 1,
+        });
+
+        // 对获得的用户名和密码进行判定
+        if (username === '') {
+            message.error('用户名为空！');
+        } else if (password === '') {
+            message.error('密码为空！');
+        } else if (password !== '202cb962ac59075b964b07152d234b70') {
+            message.error('用户名或密码错误！');
+        } else {
+            message.loading('正在登录...', () => {
+                message.destroy()
+                message.success('登录成功！', () => {
+                    // 这里传 false 表示登录成功，离开登录页面
+                    PubSub.publish('loginSuccessFlag', false)
+                    PubSub.publish('username', username)
+                })
+            });
         }
     }
 
     render() {
         return (
-            <div className="login-container" id="login-container">
-                <div className="login login-title">登录</div>
-                <div className="login login-username">
-                    <Input onChange={this.updateUsername} placeholder="用户名" prefix={<UserOutlined />} />
-                </div>
-                <div className="login login-password">
-                    <Input.Password onChange={this.updatePassword} placeholder="密码" />
-                </div>
-                <div className="login login-btn">
-                    <Button onClick={this.login} type="primary" shape="round" size="large">登录</Button>
+            <div className="login-container">
+                <div className="login-form">
+                    <div className="login-title">登录</div>
+                    <div className="login login-username">
+                        <Input onChange={this.updateUsername} placeholder="用户名" prefix={<UserOutlined />} />
+                    </div>
+                    <div className="login login-password">
+                        <Input.Password onChange={this.updatePassword} placeholder="密码" />
+                    </div>
+                    <div className="login-btn">
+                        <Button onClick={this.login} type="primary" shape="round" size="large">登录</Button>
+                    </div>
                 </div>
             </div>
         )
